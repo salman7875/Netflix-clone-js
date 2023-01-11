95781311df75b18daf9d351fb6943923ddddcfdf
Salman <asalman7875@gmail.com>
2023-01-11 09:26:00 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const btnAction = document.querySelectorAll('.btn--action')
+
+const btnOpen = document.querySelectorAll('.btn--open')
+const btnClose = document.querySelectorAll('.btn--close')
+
+btnOpen.forEach(btn => {
+  btn.addEventListener('click', function (e) {
+    const containerItem = document.querySelector('.item')
+    btn.style.transform = 'rotate(90deg)'
+    containerItem.classList.toggle('open')
+  })
+})
